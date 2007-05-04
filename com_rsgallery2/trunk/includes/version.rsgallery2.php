3d100e15bba531f28f086489afc7a007b88a7274
jonah <jonah@179cf1bb-832e-0410-8d33-ab44d92326cd>
2007-05-04 18:02:58 +0000

    changed svn revision in version.rsgallery2.php for automatic substitution in nightly builds

--- com_rsgallery2/trunk/includes/version.rsgallery2.php
+++ com_rsgallery2/trunk/includes/version.rsgallery2.php
@@ -25,7 +25,7 @@ class rsgalleryVersion {
     /** @var int Sub Release Level */
     var $DEV_LEVEL  = '2';
     /** @var int build Number */
-    var $BUILD      = 'SVN: 003(702)';
+    var $BUILD      = 'SVN: $GlobalRev$';
     /** @var string Codename */
     var $CODENAME   = '';
     /** @var string Date */
