070a1e173b39b7fc85393f78d33c424c16172c47
jonah <jonah@179cf1bb-832e-0410-8d33-ab44d92326cd>
2007-05-17 02:12:18 +0000

    added more params

--- mod_rsg2_toddFlash/trunk/mod_rsg2_toddFlash.php
+++ mod_rsg2_toddFlash/trunk/mod_rsg2_toddFlash.php
@@ -15,21 +15,38 @@ defined( '_VALID_MOS' ) or die( 'Access Denied' );
 global $mosConfig_absolute_path, $mosConfig_live_site;
 require_once($mosConfig_absolute_path.'/administrator/components/com_rsgallery2/init.rsgallery2.php');
 
-// gallery id to show
-$gid = $params->get( 'gid' );
+// dimensions of flash player
 $width = $params->get( 'width' );
 $height = $params->get( 'height' );
+
+// bunch more parameters
+$p = '';
+$p .= buildParamString( 'gid' );
+$p .= buildParamString( 'imageSize' );
+$p .= buildParamString( 'timer' );
+$p .= buildParamString( 'order' );
+$p .= buildParamString( 'looping' );
+$p .= buildParamString( 'fadeTime' );
+$p .= buildParamString( 'xpos' );
+$p .= buildParamString( 'ypos' );
+
+function buildParamString( $name ){
+	if( $params->get( $name ))
+		return "%26$name%3D" . $params->get( $name );
+	else
+		return '';
+}
 
 switch( $params->get( 'xmlTemplate' )) {
 	case 'todd':
 		echo <<<EOD
 <object
 	type="application/x-shockwave-flash"
-	data="$mosConfig_live_site/components/com_rsgallery2/flash/todd_dominey.swf?xmlName=/index.php%3Foption%3Dcom_rsgallery2%26task%3Dxml%26xmlTemplate%3Dtodd_dominey%26gid%3D$gid"
+	data="$mosConfig_live_site/components/com_rsgallery2/flash/todd_dominey.swf?xmlName=/index.php%3Foption%3Dcom_rsgallery2%26task%3Dxml%26xmlTemplate%3Dtodd_dominey$p"
 	width="$width" height="$height"
 	align="middle">
 	<param name="allowScriptAccess" value="sameDomain" />
-	<param name="movie" value="$mosConfig_live_site/components/com_rsgallery2/flash/todd_dominey.swf?xmlName=/index.php%3Foption%3Dcom_rsgallery2%26task%3Dxml%26xmlTemplate%3Dtodd_dominey%26gid%3D$gid" />
+	<param name="movie" value="$mosConfig_live_site/components/com_rsgallery2/flash/todd_dominey.swf?xmlName=/index.php%3Foption%3Dcom_rsgallery2%26task%3Dxml%26xmlTemplate%3Dtodd_dominey$p" />
 	<param name="quality" value="high" />
 	<param name="bgcolor" value="#ffffff" />
 </object>
