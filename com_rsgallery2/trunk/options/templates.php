042a141138736b8f8b19c4dc2b8435e6149a410a
jonah <jonah@179cf1bb-832e-0410-8d33-ab44d92326cd>
2007-06-14 15:39:53 +0000

    moved class rsgInstallerTemplate from includes/gallery.class.php to admin/options/templates.php

--- com_rsgallery2/trunk/options/templates.php
+++ com_rsgallery2/trunk/options/templates.php
@@ -64,6 +64,95 @@ switch ($task) {
         break;
 }
 
+
+require_once( $mosConfig_absolute_path .'/administrator/components/com_installer/installer.class.php' );
+/**
+* RSGTemplate installer, based on the Joomla installer
+* @package RSGallery2
+* @author Ronald Smit <ronald.smit@rsdev.nl>
+*/
+class rsgInstallerTemplate extends mosInstaller {
+	/**
+	* Custom install method
+	* @param boolean True if installing from directory
+	*/
+	function install( $p_fromdir = null ) {
+		global $mosConfig_absolute_path,$database;
+
+		if (!$this->preInstallCheck( $p_fromdir, 'template' )) {
+			return false;
+		}
+
+		$xmlDoc 	=& $this->xmlDoc();
+		$mosinstall =& $xmlDoc->documentElement;
+
+		// Set some vars
+		$e = &$mosinstall->getElementsByPath( 'name', 1 );
+		$this->elementName($e->getText());
+		$this->elementDir(mosPathName( JPATH_RSGALLERY2_SITE . DS . "templates" . DS . strtolower( str_replace( " ","_",$this->elementName() ) ) ) );
+		if (!file_exists( $this->elementDir() ) && !mosMakePath( $this->elementDir() )) {
+			$this->setError(1, 'Failed to create directory "' . $this->elementDir() . '"' );
+			return false;
+		}
+
+		if ($this->parseFiles( 'files' ) === false) {
+			return false;
+		}
+		if ($this->parseFiles( 'images' ) === false) {
+			return false;
+		}
+		if ($this->parseFiles( 'css' ) === false) {
+			return false;
+		}
+		if ($this->parseFiles( 'media' ) === false) {
+			return false;
+		}
+		if ($e = &$mosinstall->getElementsByPath( 'description', 1 )) {
+			$this->setError( 0, $this->elementName() . '<p>' . $e->getText() . '</p>' );
+		}
+		//Copy XML file to template directory
+		return $this->copySetupFile('front');
+	}
+	/**
+	* Custom install method
+	* @param int The id of the module
+	* @param string The URL option
+	* @param int The client id
+	*/
+	function uninstall( $id, $option, $client=0 ) {
+		global $database, $mosConfig_absolute_path;
+		echo $id;
+		if ($id == "default") {
+			echo "Niet gelukt. ($id)";
+			HTML_RSGallery::showInstallMessage( 'Cannot delete default template', 'Uninstall -  error',
+				$this->returnTo( $option ) );
+		} else {
+			// Delete directories
+			$path = JPATH_RSGALLERY2_SITE . '/templates/' . $id;
+	
+			$id = str_replace( '..', '', $id );
+			if (trim( $id )) {
+				if (is_dir( $path )) {
+					return deldir( mosPathName( $path ) );
+				} else {
+					HTML_RSGallery::showInstallMessage( 'Directory does not exist, cannot remove files', 'Uninstall -  error',
+						$this->returnTo( $option ) );
+				}
+			} else {
+				HTML_RSGallery::showInstallMessage( 'Template id is empty, cannot remove files', 'Uninstall -  error',
+					$this->returnTo( $option ) );
+				exit();
+			}			
+		}
+	}
+	/**
+	* return to method
+	*/
+	function returnTo( $option ) {
+		return "index2.php?option=com_rsgallery2&task=templates";
+	}
+}
+
 /**
 * Compiles a list of installed, templates
 *
