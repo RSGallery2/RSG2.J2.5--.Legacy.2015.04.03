f727cc2331ce5112649b72a169807bd961e285b6
grndlvl <grndlvl@179cf1bb-832e-0410-8d33-ab44d92326cd>
2007-08-14 22:56:59 +0000

    merged xspf and xspf_Music_Player

---
+++ templates/xspf/index.php
@@ -0,0 +1,55 @@
+<?php
+/**
+* This file contains the main template file for RSGallery2.
+* @package RSGallery2
+* @copyright (C) 2003 - 2006 RSGallery2
+* @license http://www.gnu.org/copyleft/gpl.html GNU/GPL
+* RSGallery is Free Software
+*/
+
+/**
+
+ATTENTION!
+
+This is built to imitate the Joomla 1.0.* style of templating.  Hopefully that is enlightening.
+
+**/
+
+defined( '_VALID_MOS' ) or die( 'Restricted Access' );
+
+// bring in display code
+require_once( JPATH_RSGALLERY2_SITE . DS . 'templates' . DS . 'semantic' . DS . 'display.class.php');
+$templatePath = JPATH_RSGALLERY2_SITE . DS . 'templates' . DS . 'xspf';
+require_once( $templatePath . DS . 'display.class.php');
+
+$rsgDisplay = new rsgDisplay_xspf();
+
+global $mosConfig_live_site;
+$template_dir = "$mosConfig_live_site/components/com_rsgallery2/templates/". $rsgConfig->get('template');
+
+//Load overlib routine for Tooltips
+mosCommonHTML::loadOverlib();
+
+
+//include page navigation
+require_once(JPATH_ROOT.'/includes/pageNavigation.php');
+
+
+$rsgDisplay->metadata();
+// append to Joomla's pathway
+$rsgDisplay->showRSPathWay();
+?>
+
+<!-- <link href="<?php //echo $template_dir ?>/css/template.css" rel="stylesheet" type="text/css" /> -->
+
+<table border="0" width="100%">
+<tr>
+    <td valign="top">
+    
+    <?php   
+     // show the main page being requested
+     $rsgDisplay->mainPage();
+    ?>
+    </td>
+</tr>
+</table>
