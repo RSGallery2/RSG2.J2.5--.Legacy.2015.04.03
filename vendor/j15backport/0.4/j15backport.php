2c7179ce72358edf33c4f4da57c5f24b1717ec6d
jonah <jonah@179cf1bb-832e-0410-8d33-ab44d92326cd>
2007-10-03 20:25:20 +0000

    Tag vendor/j15backport/current as vendor/j15backport/0.4.

---
+++ vendor/j15backport/0.4/j15backport.php
@@ -0,0 +1,57 @@
+<?php
+/**
+* This file contains all emulation for Joomla 1.5.
+* If the component is migrated to Joomla 1.5, we won't need this anymore
+* @version 0.1
+* @package RSGallery2
+* @copyright (C) 2003 - 2006 RSGallery2
+* @license http://www.gnu.org/copyleft/gpl.html GNU/GPL
+* RSGallery is Free Software
+*/
+
+defined( '_VALID_MOS' ) or die( 'Direct Access to this location is not allowed.' );
+
+//Prepare for Joomla 1.5
+if( !defined( '_JEXEC' )){
+    	// indicate we are emulating Joomla 1.5
+	define( 'JEMU15', 1 );
+	
+	// setup a Joomla 1.5 environment
+	define( '_JEXEC', 1 );
+	define ('DS', "/");
+	define( 'JPATH_BASE', $mosConfig_absolute_path );
+	define( 'JPATH_ROOT', $mosConfig_absolute_path );
+	define( 'JPATH_LIBRARIES', dirname( __FILE__ ) . '/libraries' );
+	define( 'JPATH_THEMES', JPATH_ROOT . DS . 'templates' );
+}
+
+// joomla 1.5 lib imports
+if( defined( 'JEMU15' )){
+	if ( file_exists(JPATH_LIBRARIES . DS . 'loader.php') ) {
+		require_once(JPATH_LIBRARIES . DS . 'loader.php');
+	}
+
+	jimport( 'joomla.methods' );
+	jimport( 'joomla.base.object' );
+	jimport( 'joomla.environment.request' );
+	// Include object abstract class
+	jimport( 'joomla.utilities.compat.compat' );
+
+	// Joomla! library imports
+	jimport( 'joomla.environment.response'   );
+	jimport( 'joomla.application.application' );
+	jimport( 'joomla.application.helper' );
+	jimport( 'joomla.application.menu' );
+	jimport( 'joomla.database.table' );
+	jimport( 'joomla.user.user');
+	jimport( 'joomla.environment.uri' );
+	jimport( 'joomla.factory' );
+	jimport( 'joomla.html.html' );
+	jimport( 'joomla.html.parameter' );
+	jimport( 'joomla.utilities.array' );
+	jimport( 'joomla.utilities.error' );
+	jimport( 'joomla.utilities.utility' );
+	jimport( 'joomla.utilities.string' );
+	jimport( 'joomla.version' );
+}
+?>
