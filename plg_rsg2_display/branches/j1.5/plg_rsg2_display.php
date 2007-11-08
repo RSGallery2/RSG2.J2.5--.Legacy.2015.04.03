9485109c85e89e98d0f22d54812812eedd6b6f36
jonah <jonah@179cf1bb-832e-0410-8d33-ab44d92326cd>
2007-11-08 15:23:55 +0000

    changes from jonathan to work in J1.5

--- plg_rsg2_display/branches/j1.5/plg_rsg2_display.php
+++ plg_rsg2_display/branches/j1.5/plg_rsg2_display.php
@@ -1,72 +1,54 @@
 <?php
 /**
-* @version $Id $
-* @package Joomla
-* @copyright Copyright (C) 2005 Open Source Matters. All rights reserved.
-* @license http://www.gnu.org/copyleft/gpl.html GNU/GPL, see LICENSE.php
-* Joomla! is free software and parts of it may contain or be derived from the
-* GNU General Public License or other free or open source software licenses.
-* See COPYRIGHT.php for copyright notices and details.
-*/
- 
-/** ensure this file is being included by a parent file */
-defined( '_VALID_MOS' ) or die( 'Direct Access to this location is not allowed.' );
- 
+ * @version		$Id$
+ * @package		RSGallery2
+ * @subpackage	Content
+ * @copyright	Copyright (C) 2007 Ground Level Solutions. All rights reserved.
+ * @license		GNU/GPL, see LICENSE.php
+ * Joomla! is free software. This version may have been modified pursuant
+ * to the GNU General Public License, and as distributed it includes or
+ * is derivative of works licensed under the GNU General Public License or
+ * other free or open source software licenses.
+ * See COPYRIGHT.php for copyright notices and details.
+ */
 
-$_MAMBOTS->registerFunction( 'onPrepareContent', 'botMosRSGdisplay' );
- 
-/**
-* Link bot
-*
-* <b>Usage:</b>
-* <code>{moslink id="the_id"}</code>
-*/
-function botMosRSGdisplay( $published, &$row, $mask=0, $page=0  ) {
-  global $mosConfig_absolute_path;
- 
-  if (!$published) {
-    return true;
-  }
+// Check to ensure this file is included in Joomla!
+defined( '_JEXEC' ) or die();
+$mainframe->registerEvent( 'onPrepareContent', 'plg_rsg2_display' );
+
+function plg_rsg2_display( &$row, &$params, $page=0 ) {
+  $plugin =& JPluginHelper::getPlugin('content', 'plg_rsg2_display');
  
   // define the regular expression for the bot
   $regex = "#{rsg2_display\:*(.*?)}#s";
  
   // perform the replacement
-  $row->text = preg_replace_callback( $regex, 'bot_rsg2_display_replacer', $row->text );
+  $row->text = preg_replace_callback( $regex, 'plg_rsg2_display_replacer', $row->text );
  
   return true;
 }
-/**
- * Replace {mosrsgdisplay ID,TEMPLATE} with the gallery displayed with the specified template
- *
- * @param array $matches
- * @return string
- */
-function bot_rsg2_display_replacer( &$matches ) {
-	if( !$matches )
+
+function plg_rsg2_display_replacer ( $matches ) {
+  if( !$matches )
 		return '';
 
 	global $mosConfig_absolute_path;
 	require_once( $mosConfig_absolute_path.'/administrator/components/com_rsgallery2/init.rsgallery2.php' );
-	
+
 	$attribs = explode(",",$matches[1]);
-	
 	ob_start();
 	rsgInstance::instance(
 		array(
-			'rsgTemplate' => bot_rsg2_display_clean_data($attribs[1]),
-			'gid' => bot_rsg2_display_clean_data($attribs[0])
+			'rsgTemplate' => plg_rsg2_display_replacer_clean_data( $attribs[1] ),
+			'gid' => plg_rsg2_display_replacer_clean_data( $attribs[0] )
 		)
 	);
 	$content_output = ob_get_contents();
 	ob_end_clean();	
-		
 	return $content_output;
 }
-
-function bot_rsg2_display_clean_data ( $attrib ) {//remove &nbsp; and trim white space
+function plg_rsg2_display_replacer_clean_data ( $attrib ) {//remove &nbsp; and trim white space
 	$attrib = str_replace( "&nbsp;", '', "$attrib" );
 
 	return trim( $attrib );
 }
-?>
