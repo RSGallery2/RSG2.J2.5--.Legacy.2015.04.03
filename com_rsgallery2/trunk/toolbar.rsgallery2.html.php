1271af9d44073e2ccde8675fa2dfc9cd6db13038
jonah <jonah@179cf1bb-832e-0410-8d33-ab44d92326cd>
2007-11-11 21:23:58 +0000

    removed nonworking delete template button from template manager

--- com_rsgallery2/trunk/toolbar.rsgallery2.html.php
+++ com_rsgallery2/trunk/toolbar.rsgallery2.html.php
@@ -20,7 +20,10 @@ class menu_rsg2_templates{
 		mosMenuBar::spacer();
 		//mosMenuBar::assign();
 		mosMenuBar::spacer();
-		mosMenuBar::deleteList();
+
+		// deleting isn't working at the moment.  removing button for release.
+// 		mosMenuBar::deleteList();
+
 		mosMenuBar::spacer();/*
 		mosMenuBar::editHtmlX( 'edit_main', 'Main' );
 		mosMenuBar::spacer();
