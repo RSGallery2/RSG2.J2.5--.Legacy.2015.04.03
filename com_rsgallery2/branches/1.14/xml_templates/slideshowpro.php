152986b437933c9af2b3dfe2111024210218b9fe
jonah <jonah@179cf1bb-832e-0410-8d33-ab44d92326cd>
2007-11-12 20:11:01 +0000

    creating 1.14 branch

---
+++ com_rsgallery2/branches/1.14/xml_templates/slideshowpro.php
@@ -0,0 +1,63 @@
+<?php
+
+/**
+* xml file for Slide Show Pro http://www.slideshowpro.net/
+* @package RSGallery2
+* @author Anon. <josiah@WhaleHosting.ca>
+*/
+
+class rsgXmlGalleryTemplate_slideshowpro extends rsgXmlGalleryTemplate_generic{
+    function getName(){
+        return 'SlideShowPro Template';
+    }
+    
+    function prepare(){
+        global $rsgConfig, $mosConfig_live_site;
+        $imgPath = $rsgConfig->get("imgPath_display");
+        $thumbPath = $rsgConfig->get("imgPath_thumb");
+
+        $this->output = "\n";
+        $this->output .= "<gallery>";
+        //$this->output .= "<br />";
+        
+        foreach( $this->gallery->kids() as $kid ){
+            $title = htmlentities($kid->get("name"));
+		    $descr = htmlentities($kid->get("description"));
+		    
+		    // get  thumbnail item for gallery
+		    $thumb = $kid->thumb();
+		    if( is_a($thumb, "rsgItem_image")){
+				// get thumbnail resource of image item
+				$thumb = $thumb->thumb();
+				// get full url of thumbnail
+				$thumb = $thumb->url();
+		    }
+		    else $thumb = '';
+		    
+			$this->output .= "<album id='$title' title='$title' description='$descr' lgPath='$mosConfig_live_site/$imgPath/' tnPath='$mosConfig_live_site/$thumbPath/' tn='$thumb'>";  
+
+
+            foreach( $kid->items() as $img ){
+            	if(!is_a($img, "rsgItem_image")) continue;
+            	$imgDescr = $img->get("title");
+            	
+            	// get display resource of image item
+				$image = $img->display();
+				// name of image item
+				$image = $image->name;
+				// get just the filename
+				$image = pathinfo($image);
+				$image = $image["basename"];
+            	
+                $this->output .= '  <img src="';
+                $this->output .= "$image";
+                $this->output .= '" caption="';
+                $this->output .= $imgDescr;
+                $this->output .= "\" />\n";
+			}
+            $this->output .= "</album>";
+		}
+        $this->output .= "</gallery>";
+	}
+               
+}
