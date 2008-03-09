43e43f21481a53f3b42addcec456b17ee9b6c9ee
jonah <jonah@179cf1bb-832e-0410-8d33-ab44d92326cd>
2008-03-09 21:11:14 +0000

    ported changes from trunk.  IMPORTANT: parameter order has changed.  See description for details.

--- plg_rsg2_display/branches/j1.5/plg_rsg2_display.php
+++ plg_rsg2_display/branches/j1.5/plg_rsg2_display.php
@@ -14,6 +14,7 @@
 
 // Check to ensure this file is included in Joomla!
 defined( '_JEXEC' ) or die();
+
 $mainframe->registerEvent( 'onPrepareContent', 'plg_rsg2_display' );
 
 function plg_rsg2_display( &$row, &$params, $page=0 ) {
@@ -29,24 +30,31 @@ function plg_rsg2_display( &$row, &$params, $page=0 ) {
 }
 
 function plg_rsg2_display_replacer ( $matches ) {
-  if( !$matches )
+	if( !$matches )
 		return '';
 
 	global $mosConfig_absolute_path;
 	require_once( $mosConfig_absolute_path.'/administrator/components/com_rsgallery2/init.rsgallery2.php' );
-
+	
 	$attribs = explode(",",$matches[1]);
+	
+	$newInstance = array(
+		'rsgTemplate' => bot_rsg2_display_clean_data( strtolower( array_shift( $attribs ))),
+		'gid' => (int) bot_rsg2_display_clean_data( array_shift( $attribs ))
+		);
+	
+	foreach( $attribs as $a ){
+		$attrib = explode('=',$a);
+		$newInstance[ bot_rsg2_display_clean_data( $attrib[0] )] = bot_rsg2_display_clean_data( $attrib[1] );
+	}
 	ob_start();
-	rsgInstance::instance(
-		array(
-			'rsgTemplate' => plg_rsg2_display_replacer_clean_data( $attribs[1] ),
-			'gid' => plg_rsg2_display_replacer_clean_data( $attribs[0] )
-		)
-	);
+	rsgInstance::instance( $newInstance );
 	$content_output = ob_get_contents();
 	ob_end_clean();	
+		
 	return $content_output;
 }
+
 function plg_rsg2_display_replacer_clean_data ( $attrib ) {//remove &nbsp; and trim white space
 	$attrib = str_replace( "&nbsp;", '', "$attrib" );
 
