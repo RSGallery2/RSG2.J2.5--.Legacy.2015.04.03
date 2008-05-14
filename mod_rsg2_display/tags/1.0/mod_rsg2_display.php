f7e868ee93338b846f35f4b456314d5ec6e209e0
jonah <jonah@179cf1bb-832e-0410-8d33-ab44d92326cd>
2008-05-14 19:04:34 +0000

    tagging 1.0 release

---
+++ mod_rsg2_display/tags/1.0/mod_rsg2_display.php
@@ -0,0 +1,36 @@
+<?php
+/**
+* @version		$Id: mod_rsg2.php 10079 2008-02-28 13:39:08Z ircmaxell $
+* @package		Joomla
+* @copyright	Copyright (C) 2005 - 2008 Open Source Matters. All rights reserved.
+* @license		GNU/GPL, see LICENSE.php
+* Joomla! is free software. This version may have been modified pursuant
+* to the GNU General Public License, and as distributed it includes or
+* is derivative of works licensed under the GNU General Public License or
+* other free or open source software licenses.
+* See COPYRIGHT.php for copyright notices and details.
+*/
+
+// no direct access
+defined('_JEXEC') or die('Restricted access');
+
+require_once( JPATH_ROOT.'/administrator/components/com_rsgallery2/init.rsgallery2.php' );
+
+$newInstance = array(
+	'rsgTemplate' => $params->get( 'rsgTemplate' ),
+	'gid' => $params->get( 'gid' )
+	);
+
+// get extra params
+for( $i=0; $i<5; $i++ ){
+	$p = $params->get( "extra$i" );
+
+	if( ! $p )
+		continue;
+
+	$p = explode( '=', $p );
+	$newInstance[ trim($p[0]) ] = trim($p[1]);
+}
+
+// execute (display) the rsg2 instance
+rsgInstance::instance( $newInstance );
