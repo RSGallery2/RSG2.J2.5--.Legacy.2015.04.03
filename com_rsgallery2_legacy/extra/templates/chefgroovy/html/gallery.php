29766791d8c478bccc495d9cce7f2a5427092cf9
chefgroovy <chefgroovy@179cf1bb-832e-0410-8d33-ab44d92326cd>
2008-07-23 21:25:02 +0000

    Legacy: + template based on semantics. 2 column gallery view and beginning of tagging system

---
+++ com_rsgallery2_legacy/extra/templates/chefgroovy/html/gallery.php
@@ -0,0 +1,97 @@
+<?php defined('_JEXEC') or die('Restricted access'); ?>
+
+<?php
+
+global $ItemId;
+
+//Show My Galleries link
+if ($rsgConfig->get('show_mygalleries')) {
+	echo $this->showRsgHeader();
+}
+//show search box
+$this->showSearchBox();
+
+//Show introduction text
+?>
+<div class="intro_text"><?php echo $this->gallery->description; ?></div>
+<?php
+//Show limitbox
+if( $this->pageNav ):
+?>
+	<div class="rsg2-pagenav-limitbox">
+		<?php echo $this->getGalleryLimitBox(); ?>
+	</div>
+<?php
+endif;
+?>
+
+<table width="100%" border="1">
+  
+<?php
+$i=0;
+foreach( $this->kids as $kid ):
+++$i;
+
+if ($i % 2)  {
+	echo "<tr>";
+}
+
+?>
+
+
+<td>
+<div class="rsg_galleryblock">
+	<div class="rsg2-galleryList-status"><?php echo $kid->status;?></div>
+	<div class="rsg2-galleryList-thumb">
+		<?php echo $kid->thumbHTML; ?>
+	</div>
+	<div class="rsg2-galleryList-text">
+		<?php echo $kid->galleryName;?>
+		<span class='rsg2-galleryList-newImages'>
+			<sup><?php if( $this->gallery->hasNewImages() ) echo _RSGALLERY_NEW; ?></sup>
+		</span>
+		<?php echo $this->_showGalleryDetails( $kid );?>
+		<div class="rsg2-galleryList-description"><?php echo $kid->description;?>
+		</div>
+	</div>
+	<div class="rsg_sub_url_single">
+		<?php $this->_subGalleryList( $kid ); ?>
+	</div>
+</div>
+</td>
+
+<?php
+if ( $i % 2 == 0)  {
+	echo "</tr>";
+}
+
+?>
+
+<?php
+endforeach;
+?>
+
+</table>
+
+
+<div class="rsg2-clr"></div>
+<?php
+if($this->gallery->id == 0){
+
+	// show random and latest only in the top gallery 
+	
+	//Show block with random images 
+	$this->showImages("random", 3);
+	//Show block with latest images
+	$this->showImages("latest", 3);
+}
+if( $this->pageNav ):
+?>
+
+<div class="rsg2-pageNav">
+	<?php echo $this->getGalleryPageLinks(); ?>
+	<br/>
+	<?php echo $this->getGalleryPagesCounter(); ?>
+</div>
+<div class='rsg2-clr'>&nbsp;</div>
+<?php endif; ?>
