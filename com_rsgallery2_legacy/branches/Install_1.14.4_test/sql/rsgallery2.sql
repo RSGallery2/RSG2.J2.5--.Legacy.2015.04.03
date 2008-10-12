40e635b5f6291fabbeb500e413c5d07d40343c18
chefgroovy <chefgroovy@179cf1bb-832e-0410-8d33-ab44d92326cd>
2008-10-12 00:49:05 +0000

    Almost.... :)

--- com_rsgallery2_legacy/branches/Install_1.14.4_test/sql/rsgallery2.sql
+++ com_rsgallery2_legacy/branches/Install_1.14.4_test/sql/rsgallery2.sql
@@ -91,3 +91,37 @@ CREATE TABLE IF NOT EXISTS `#__rsgallery2_acl` (
   `registered_vote_vote` tinyint( 1 ) NOT NULL default '1',
   PRIMARY KEY  (`id`)
 ) TYPE=MyISAM;
+
+
+# DROP TABLE IF EXISTS `#__rsgallery2_tags`;
+CREATE TABLE IF NOT EXISTS `#__rsgallery2_tags` (
+  `id`                int AUTO_INCREMENT NOT NULL,
+  `name`              varchar(255) NOT NULL,
+  `enabled`           tinyint(1) NOT NULL DEFAULT '0',
+  `description`       varchar(255) NOT NULL,
+  `date_added`        datetime NOT NULL DEFAULT '0000-00-00 00:00:00',
+  `published`         tinyint(1) NOT NULL DEFAULT '0',
+  `checked_out`       int UNSIGNED NOT NULL DEFAULT '0',
+  `checked_out_time`  datetime NOT NULL DEFAULT '0000-00-00 00:00:00',
+  `user`              tinyint NOT NULL DEFAULT '0',
+  `uid`               int UNSIGNED NOT NULL DEFAULT '0',
+  `ordering`          int NOT NULL DEFAULT '0',
+  `params`            text NOT NULL,
+  /* Keys */
+  PRIMARY KEY (`id`)
+) TYPE = MyISAM
+  CHARACTER SET `latin1` COLLATE `latin1_swedish_ci`;
+  
+  
+
+
+# DROP TABLE IF EXISTS `#__rsgallery2_tagmatch`;
+CREATE TABLE IF NOT EXISTS `#__rsgallery2_tagmatch` (
+  `match_id`         int AUTO_INCREMENT NOT NULL,
+  `image_id`         int NOT NULL DEFAULT '0',
+  `tag_id`           int NOT NULL DEFAULT '0',
+  `date_match_made`  datetime,
+  /* Keys */
+  PRIMARY KEY (`match_id`)
+) TYPE = MyISAM
+  CHARACTER SET `latin1` COLLATE `latin1_swedish_ci`;
