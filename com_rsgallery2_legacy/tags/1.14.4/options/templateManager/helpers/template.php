f671794459f4c08afadd668e83f5e784c9e4fc63
chefgroovy <chefgroovy@179cf1bb-832e-0410-8d33-ab44d92326cd>
2008-10-18 02:17:18 +0000

    Tag release for Legacy 1.14.4

---
+++ com_rsgallery2_legacy/tags/1.14.4/options/templateManager/helpers/template.php
@@ -0,0 +1,82 @@
+<?php
+/**
+ * RSGallery2 template helper class
+ * Derived from Joomla 1.5 TemplateHelper
+ * @author John Caprez <john@swizzysoft.com>
+ * @package RSGallery2
+ */
+
+// Check to ensure this file is within the rest of the framework
+defined('_JEXEC') or die('Direct Access to this location is not allowed.');
+
+/**
+ *
+ */
+class TemplatesHelper
+{
+	function isTemplateDefault($template)
+	{
+		global $rsgConfig;
+		$currentTemplate = $rsgConfig->get('template');
+		return $currentTemplate == $template ? 1 : 0;
+	}
+
+
+	function parseXMLTemplateFiles($templateBaseDir)
+	{
+		// Read the template folder to find templates
+		jimport('joomla.filesystem.folder');
+		$templateDirs = JFolder::folders($templateBaseDir);
+
+		$rows = array();
+
+		// Check that the directory contains an xml file
+		foreach ($templateDirs as $templateDir)
+		{
+			if(!$data = TemplatesHelper::parseXMLTemplateFile($templateBaseDir, $templateDir)){
+				continue;
+			} else {
+				$rows[] = $data;
+			}
+		}
+
+		return $rows;
+	}
+
+	function parseXMLTemplateFile($templateBaseDir, $templateDir)
+	{
+		// Check of the xml file exists
+		if(!is_file($templateBaseDir.DS.$templateDir.DS.'templateDetails.xml')) {
+			return false;
+		}
+		$xml = JApplicationHelper::parseXMLInstallFile($templateBaseDir.DS.$templateDir.DS.'templateDetails.xml');
+		
+		if ($xml['type'] == 'template') {
+		// *** REMOVING FOR NOW, IT BREAKS Joomla 1.13.x +
+			
+		//	JError::raiseWarning(0,"The type attribute in ".$templateBaseDir.DS.$templateDir.DS."templateDetails.xml has a not supported value.<br/>Please change it to 'rsgTemplate'.","");
+
+		echo ("The type attribute in ".$templateBaseDir.DS.$templateDir.DS."templateDetails.xml has a not supported value.<br/>Please change it to 'rsgTemplate'");
+
+			return false;
+		}
+		
+		if ($xml['type'] != 'rsgTemplate' ) {
+			return false;
+		}
+
+		$data = new StdClass();
+		$data->directory = $templateDir;
+
+		foreach($xml as $key => $value) {
+			$data->$key = $value;
+		}
+
+		$data->checked_out = 0;
+		$data->mosname = JString::strtolower(str_replace(' ', '_', $data->name));
+
+		return $data;
+	}
+
+}
+?>
