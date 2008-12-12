2e114a6eda5419e6c7ae8e8c19ef33be146c6ed3
swissjohn <swissjohn@179cf1bb-832e-0410-8d33-ab44d92326cd>
2008-12-12 21:56:52 +0000

    minor fix in semantic template

--- com_rsgallery2/trunk/site/templates/semantic/html/item_image.php
+++ com_rsgallery2/trunk/site/templates/semantic/html/item_image.php
@@ -4,20 +4,23 @@
 	$item = $this->currentItem;
 	$watermark = $rsgConfig->get('watermark');
 
-	$imageUrl = $watermark ? waterMarker::showMarkedImage( $item->name ) : 
+	$imageOriginalUrl = $watermark ? waterMarker::showMarkedImage( $item->name, 'original' ) : 
 		imgUtils::getImgOriginal( $item->name );
+		
+	$imageUrl = $watermark ? waterMarker::showMarkedImage( $item->name ) : 
+		imgUtils::getImgDisplay( $item->name );
 
 	switch ($rsgConfig->get('displayPopup')) {
 		//No popup
 		case 0:{
 					?>
 					<img class="rsg2-displayImage" src="<?php echo $imageUrl;?>" alt="<?php echo $item->name; ?>" title="<?php echo $item->descr; ?>" />
-			<?php
-			break;
-		}
-		//Normal popup
-		case 1:{
-					?><a href="<?php echo $imageUrl; ?>" target="_blank">
+		<?php
+		break;
+	}
+	//Normal popup
+	case 1:{
+					?><a href="<?php echo $imageOriginalUrl; ?>" target="_blank">
 					<img class="rsg2-displayImage" src="<?php echo $imageUrl;?>" alt="<?php echo $item->name; ?>" title="<?php echo $item->descr; ?>" />
 					</a>
 			<?php
@@ -25,14 +28,14 @@
 		}
 		case 2:{
 			JHTML::_('behavior.modal');
-			$jsModal = '
-					window.addEvent("domready", function() {
-					SqueezeBox.initialize({});
-					var img = $$("img.rsg2-displayImage")[0];
-					img.addEvent("click", function(e)
-					{
-					new Event(e).stop();
-					SqueezeBox.fromElement(img,{url:"' . $imageUrl .'", 
+		$jsModal = '
+				window.addEvent("domready", function() {
+				SqueezeBox.initialize({});
+				var img = $$("img.rsg2-displayImage")[0];
+				img.addEvent("click", function(e)
+				{
+				new Event(e).stop();
+				SqueezeBox.fromElement(img,{url:"' . $imageOriginalUrl .'", 
 					classWindow:"rsg2", 
 					classOverlay:"rsg2",
 					onOpen:function(img){
