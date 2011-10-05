b1df1f853c0e3df165137e461aad543f05f57e67
mirjam <mirjam@179cf1bb-832e-0410-8d33-ab44d92326cd>
2011-10-05 13:20:27 +0000

    Tagging SVN1046 v3 branch to 3.0.1.

---
+++ com_rsgallery2/tags/3.0.1/admin/options/maintenance.php
@@ -0,0 +1,441 @@
+<?php
+/**
+* Maintenance options for RSGallery2
+* @version $Id$
+* @package RSGallery2
+* @copyright (C) 2003 - 2011 RSGallery2
+* @license http://www.gnu.org/copyleft/gpl.html GNU/GPL
+* RSGallery is Free Software
+*/
+
+// no direct access
+defined( '_JEXEC' ) or die( 'Restricted access' );
+
+require_once( $rsgOptions_path . 'maintenance.html.php' );
+require_once( $rsgOptions_path . 'maintenance.class.php' );
+
+// Only those with core.manage can get here via $rsgOption = maintenance
+// Check if core.admin is allowed
+if (!JFactory::getUser()->authorise('core.admin', 'com_rsgallery2')) {
+	JError::raiseWarning(404, JText::_('JERROR_ALERTNOAUTHOR'));
+	return;
+} else {
+	$cid = JRequest::getVar("cid", array(), 'default', 'array' );
+	$task = JRequest::getVar( 'task', null);
+
+	switch ($task) {
+		/* Regenerate thumbs calls */
+		case 'regenerateThumbs':
+			HTML_RSGALLERY::RSGalleryHeader('cpanel', JText::_('COM_RSGALLERY2_MAINT_REGEN'));
+			regenerateImages();
+			HTML_RSGALLERY::RSGalleryFooter();
+			break;
+		case 'executeRegenerateThumbImages':
+			executeRegenerateThumbImages();
+			break;
+		case 'executeRegenerateDisplayImages';
+			executeRegenerateDisplayImages();
+			break;
+		/* Consolidate database calls */
+		case 'consolidateDB':
+			consolidateDB();
+			break;
+		case 'createImages':
+			createImages();
+			break;
+		case 'deleteImages':
+			deleteImages();
+			break;
+		case 'createDbEntries':
+			createDbEntries();
+			break;
+			
+		/* Optimize DB calls*/
+		case 'optimizeDB':
+			optimizeDB();
+			break;
+		
+		/* Migration calls */
+		case 'showMigration':
+			HTML_RSGALLERY::RSGalleryHeader('cpanel', JText::_('COM_RSGALLERY2_MIGRATION_OPTIONS'));
+			showMigration();
+			HTML_RSGALLERY::RSGalleryFooter();
+			break;
+		case 'doMigration':
+			doMigration();
+			break;
+			
+		case 'test':
+			test();
+			break;
+		default:
+			HTML_RSGALLERY::RSGalleryHeader('cpanel', JText::_('COM_RSGALLERY2_MAINT_HEADER'));
+			showMaintenanceCP( $option );
+			HTML_RSGALLERY::RSGalleryFooter();
+			break;
+	}	// end task switch
+}
+
+function test() {
+	// http://JOOMLA/administrator/index.php?option=com_rsgallery2&rsgOption=maintenance&task=test
+/*	$gid = 1;
+	$gallery = rsgGalleryManager::_get( $gid );
+    $images = $gallery->items();
+    foreach ($images as $image) {
+    	$imgname[] = $image->name;
+    }
+    $image = array_rand($imgname);
+    echo "De image name is: ".$imgname[$image];
+    echo "<pre>";
+    var_dump($imgname);
+    echo "</pre>";
+*/
+   /* 
+	* Function recursiveGalleriesList gets a list of galleries with their id, parent en hierarchy level ordered by ordering and subgalleries grouped by their parent.
+	* $id		Gallery parent number
+	* $list		The list to return
+	* $children	The 2dim. array with children
+	* $maxlevel Maximum depth of levels
+	* $level	Hierarchy level (e.g. sub gallery of root is level 1)
+	* return	Array
+	*/
+	function recursiveGalleriesList(){
+		//Function to help out
+		function treerecurse($id,  $list, &$children, $maxlevel=20, $level=0) {
+			//if there are children for this id and the max.level isn't reached
+			if (@$children[$id] && $level <= $maxlevel) {
+				//add each child to the $list and ask for its children
+				foreach ($children[$id] as $v) {
+					$id = $v->id;	//gallery id
+					$list[$id] = $v;
+					$list[$id]->level = $level;
+					//$list[$id]->children = count(@$children[$id]);
+					$list = treerecurse($id,  $list, $children, $maxlevel, $level+1);
+				}
+			}
+			return $list;
+		}
+		// Get a list of all galleries (id/parent) ordered by parent/ordering
+		$database =& JFactory::getDBO();
+		$query = "SELECT `id`, `parent`, `name` "
+				. " FROM #__rsgallery2_galleries"
+			//	. " WHERE published != -2"
+				. " ORDER BY parent, ordering";
+		$database->setQuery( $query );
+		$allGalleries = $database->loadObjectList();
+		// Establish the hierarchy by first getting the children: 2dim. array $children[parentid][]
+		$children = array();
+		if ( $allGalleries ) {
+			foreach ( $allGalleries as $v ) {
+				$pt     = $v->parent;
+				$list   = @$children[$pt] ? $children[$pt] : array();
+				array_push( $list, $v );
+				$children[$pt] = $list;
+			}
+		}
+		// Get list of galleries with (grand)children in the right order and with level info
+		$recursiveGalleriesList = treerecurse( 0, array(), $children, 20, 0 );
+		return $recursiveGalleriesList;
+	}
+	$list = recursiveGalleriesList();
+	foreach ($list as $listItem) {
+		$indent = "";
+		for ($i = 0; $i < $listItem->level; $i++) {
+			$indent .= "&nbsp;&nbsp;&nbsp;";
+		}
+		echo $indent.$listItem->id.' '.$listItem->name.'<br>';
+	}
+
+
+
+}// end of function test()
+
+/**
+ * Shows Migration main screen
+ * It shows detected gallerysystem and offers a migration option
+ */
+function showMigration() {
+    require_once(JPATH_RSGALLERY2_ADMIN.'/includes/install.class.php');
+    
+    //Initialize new install instance
+    $rsgInstall = new rsgInstall();
+
+    if (isset($_REQUEST['type']))
+        $type = mosGetParam ( $_REQUEST, 'type'  , '');
+    else
+        $type = NULL;
+
+
+	if( $type=='' ) {
+		$rsgInstall->showMigrationOptions();	
+	} else {
+        $result = $rsgInstall->doMigration( $type );
+        if( $result !==true ) {
+            echo $result;
+            HTML_RSGallery::showCP();
+    	} else {
+        	echo JText::_('COM_RSGALLERY2_MIGRATION_SUCCESSFUL');
+        	HTML_RSGallery::showCP();
+    	}
+	}
+}
+
+function doMigration() {
+	$type  	= JRequest::getVar('type', null);
+	require_once(JPATH_RSGALLERY2_ADMIN.'/includes/install.class.php');
+	
+	$migrate_class = "migrate_".$type; 
+	$migrate = new $migrate_class;
+	$migrate->migrate();
+}
+
+/**
+ * Shows Control Panel for maintenance of RSGallery2
+ */
+function showMaintenanceCP() {
+	html_rsg2_maintenance::showMaintenanceCP();
+}
+
+function regenerateImages() {
+	//Select the right gallery, multiple galleries or select them all
+	$lists['gallery_dropdown'] = galleryUtils::galleriesSelectList(null, "gid[]", true, "MULTIPLE");
+	
+	html_rsg2_maintenance::regenerateImages($lists);
+}
+/**
+ * Function will regenerate thumbs for a specific gallery or set of galleries
+ * @todo Check if width really changed, else no resize needed. 
+ * Perhaps by sampling the oldest thumb from the gallery and checking dimensions against current setting. 
+ */
+function executeRegenerateThumbImages() {
+	global $rsgConfig;
+	$mainframe =& JFactory::getApplication();
+	$error = 0;
+	$gid = JRequest::getVar( 'gid', array());
+	if ( empty($gid) ) {
+		$mainframe->redirect("index.php?option=com_rsgallery2&rsgOption=maintenance&task=regenerateThumbs", JText::_('COM_RSGALLERY2_NO_GALLERY_SELECTED'));
+		return;
+	}
+
+	foreach ($gid as $id) {
+    	if ($id > 0) {
+    		//Check if resize is really needed. It takes a lot of resources when changing thumbs when dimensions did not change!
+    		if ( !rsg2_maintenance::thumbSizeChanged($id) ) {
+				$mainframe->redirect("index.php?option=com_rsgallery2&rsgOption=maintenance&task=regenerateThumbs", JText::_('COM_RSGALLERY2_THUMBNAIL_SIZE_DID_NOT_CHANGE_REGENERATION_NOT_NEEDED'));
+				return;
+			} else {
+				$gallery = rsgGalleryManager::_get($id);
+				$images = $gallery->items();
+				foreach ($images as $image) {
+					$imagename = imgUtils::getImgOriginal($image->name, true);
+					if (!imgUtils::makeThumbImage($imagename)) {
+						//Error counter
+						$error++;
+					}
+				}
+    		}
+    	}
+    }
+    if ($error > 0) {
+    	$msg = JText::_('COM_RSGALLERY2_MAINT_REGEN_ERRORS');
+    } else {
+		$msg = JText::_('COM_RSGALLERY2_MAINT_REGEN_NO_ERRORS');
+    }
+    $mainframe->redirect("index.php?option=com_rsgallery2&rsgOption=maintenance&task=regenerateThumbs", $msg);
+}
+/**
+ * Function will regenerate display images for a specific gallery or set of galleries
+ * @todo Check if width really changed, else no resize needed. 
+ */
+function executeRegenerateDisplayImages() {
+	global $rsgConfig;
+	$mainframe =& JFactory::getApplication();
+	$error = 0;
+	$gid = JRequest::getVar( 'gid', array());
+	if ( empty($gid) ) {
+		$mainframe->redirect("index.php?option=com_rsgallery2&rsgOption=maintenance&task=regenerateThumbs", JText::_('COM_RSGALLERY2_NO_GALLERY_SELECTED'));
+		return;
+	}
+
+	foreach ($gid as $id) {
+    	if ($id > 0) {
+			$gallery = rsgGalleryManager::_get($id);
+			$images = $gallery->items();
+			foreach ($images as $image) {
+				//Get full path of the original image
+				$originalImageFullPath = imgUtils::getImgOriginal($image->name, true);
+				//Get the name of the image
+				$parts = pathinfo( $originalImageFullPath );
+				$newName = $parts['basename'];
+				//Get the correct width for the display image
+				$width = getimagesize( $originalImageFullPath );
+				if( !$width ){
+					//error (no width found)
+					$mainframe->enqueueMessage(JText::sprintf('COM_RSGALLERY2_COULD_NOT_CREATE_DISPLAY_IMAGE_WITH_NOT_FOUND', $newName), $type= 'error');
+					$error++;
+				} else {
+					//the actual image width and height and its max
+					$height = $width[1];
+					$width = $width[0];
+					if ($height > $width) {
+						$maxSideImage = $height;
+					} else {
+						$maxSideImage = $width;
+					}
+					// if original is wider or higher than display size, create a display image
+					if( $maxSideImage > $rsgConfig->get('image_width') ) {
+						$result = imgUtils::makeDisplayImage( $originalImageFullPath, $newName, $rsgConfig->get('image_width') );
+					} else {
+						$result = imgUtils::makeDisplayImage( $originalImageFullPath, $newName, $maxSideImage );
+					}
+					//If creation of image failed: let user know
+					if( !$result ){
+					//	imgUtils::deleteImage( $newName );
+						$mainframe->enqueueMessage(JText::sprintf('COM_RSGALLERY2_COULD_NOT_CREATE_DISPLAY_IMAGE', $newName), $type= 'error');
+						$error++;
+					}
+				}
+			}
+    	}
+    }
+    if ($error > 0) {
+    	$msg = JText::_('COM_RSGALLERY2_MAINT_REGEN_ERRORS_DISPLAY');
+    } else {
+		$msg = JText::_('COM_RSGALLERY2_MAINT_REGEN_NO_ERRORS');
+    }
+    $mainframe->redirect("index.php?option=com_rsgallery2&rsgOption=maintenance&task=regenerateThumbs", $msg);
+}
+
+function consolidateDB() {
+	$consolidate = new rsg2_consolidate();
+	$consolidate->consolidateDB();
+}
+
+function createImages() {
+	global $rsgConfig;
+	$mainframe =& JFactory::getApplication();
+	//Check if id or name is set
+	if ( isset( $_REQUEST['id'] ) ) {
+		$id = JRequest::getInt( 'id', null);
+		$name = galleryUtils::getFileNameFromId($id);
+	}
+	elseif ( isset($_REQUEST['name'] ) ) {
+		$name    = JRequest::getVar( 'name', null);
+	} else {
+		$mainframe->redirect("index.php?option=com_rsgallery2&rsgOption=maintenance", JText::_('COM_RSGALLERY2_NO_FILEINFORMATION_FOUND_THIS_SHOULD_NEVER_HAPPEN'));
+	}
+	
+	//Just for readability of code
+	$original = JPATH_ORIGINAL . DS . $name;
+	$display  = JPATH_DISPLAY . DS . imgUtils::getImgNameDisplay($name);
+	$thumb    = JPATH_THUMB . DS . imgUtils::getImgNameThumb($name);
+	
+	//If only thumb exists, no generation possible so redirect.
+	if (!file_exists($original) AND !file_exists($display) AND file_exists($thumb) ) {
+		$mainframe->redirect("index.php?option=com_rsgallery2&rsgOption=maintenance&task=consolidateDB", JText::_('COM_RSGALLERY2_MAINT_REGEN_ONLY_THUMB'));
+		return;
+	}
+	//Go make images
+	if ( file_exists($original) ) {
+		//Check if display image exists, if not make it.
+		if (!file_exists($display)) {
+	    	imgUtils::makeDisplayImage($original, NULL, $rsgConfig->get('image_width') );
+	    }
+		if (!file_exists($thumb)) {
+	        imgUtils::makeThumbImage($original);
+	    }
+	} else {
+	    if (file_exists($display)) {
+	        copy($display, $original);
+	    }
+	    if (!file_exists($thumb)) {
+	        imgUtils::makeThumbImage($display);
+	    }
+	}
+	$mainframe->redirect("index.php?option=com_rsgallery2&rsgOption=maintenance&task=consolidateDB",$name.' '.JText::_('COM_RSGALLERY2_MAINT_REGEN_SUCCESS'));
+}
+
+function deleteImages() {
+	$mainframe =& JFactory::getApplication();
+	$database =& JFactory::getDBO();
+	$name = JRequest::getVar('name', null);
+    
+    if ( imgUtils::deleteImage( $name ) ) {
+    	$txt = JText::_('COM_RSGALLERY2_MAGE-S_DELETED_SUCCESFULLY');
+    } else {
+    	$txt = JText::_('COM_RSGALLERY2_IMAGE-S_WERE_NOT_DELETED');
+    }
+    
+    $mainframe->redirect("index.php?option=com_rsgallery2&rsgOption=maintenance&task=consolidateDB", $txt);
+}
+
+function createDbEntries() {
+	$name = JRequest::getVar('name'  , null);
+	$t_id = JRequest::getVar('t_id'  , null);
+    $gid = JRequest::getInt('gallery_id'  , null);
+    echo "<pre>";
+    print_r($name);
+    echo "</pre>";
+    echo "We are going to create an entry for $name in $t_id.";
+}
+
+/**
+ * Used in the consolidate database function
+ * Creates images based on an image id or an image name
+ */
+function regenerateImage() {
+	$mainframe =& JFactory::getApplication();
+	global $rsgConfig;
+	$database =& JFactory::getDBO();
+	
+	//Check if id or name is set
+	if ( isset( $_REQUEST['id'] ) ) {
+		$id = JRequest::getInt( 'id', null);
+		$name = galleryUtils::getFileNameFromId($id);
+	}
+	elseif ( isset($_REQUEST['name'] ) ) {
+		$name    = JRequest::getVar( 'name', null);
+	} else {
+		$mainframe->redirect("index.php?option=com_rsgallery2&task=batchupload", JText::_('COM_RSGALLERY2_NO_FILEINFORMATION_FOUND_THIS_SHOULD_NEVER_HAPPEN'));
+	}
+	
+	//Just for readability of code
+	$original = JPATH_ORIGINAL . DS . $name;
+	$display  = JPATH_DISPLAY . DS . imgUtils::getImgNameDisplay($name);
+	$thumb    = JPATH_THUMB . DS . imgUtils::getImgNameThumb($name);
+	    
+	if ( file_exists($original) ) {
+		//Check if display image exists, if not make it.
+		if (!file_exists($display)) {
+	    	imgUtils::makeDisplayImage($original, NULL, $rsgConfig->get('image_width') );
+	    }
+		if (!file_exists($thumb)) {
+	        imgUtils::makeThumbImage($original);
+	    }
+	} else {
+	    if (file_exists($display)) {
+	        copy($display, $original);
+	    }
+	    if (!file_exists($thumb)) {
+	        imgUtils::makeThumbImage($display);
+	    }
+	}
+}
+
+/**
+ * Checks database for problems and optimizes tables
+ */
+function optimizeDB() {
+	$mainframe =& JFactory::getApplication();
+	$database =& JFactory::getDBO();
+	
+	require_once(JPATH_ROOT . DS . "administrator" . DS . "components" . DS . "com_rsgallery2" . DS . "includes" . DS . "install.class.php");
+	$install = new rsgInstall();
+	$tables = $install->tablelistNew;
+	foreach ($tables as $table) {
+		$database->setQuery("OPTIMIZE $table");
+		$database->query();
+	}
+	$mainframe->redirect("index.php?option=com_rsgallery2&amp;rsgOption=maintenance",JText::_('COM_RSGALLERY2_MAINT_OPTIMIZE_SUCCESS'));
+}
+?>
---
+++ com_rsgallery2/tags/3.0.1/site/templates/semantic/display.class.php
@@ -0,0 +1,402 @@
+<?php
+/**
+ * @version $Id$
+ * @package RSGallery2
+ * @copyright (C) 2003 - 2010 RSGallery2
+ * @license http://www.gnu.org/copyleft/gpl.html GNU/GPL
+ */
+defined( '_JEXEC' ) or die( 'Restricted Access' );
+
+/**
+ * Template class for RSGallery2
+ * @package RSGallery2
+ * @author Ronald Smit <ronald.smit@rsdev.nl>
+ */
+class rsgDisplay_semantic extends rsgDisplay{
+
+	function inline(){
+		$this->display( 'inline.php' );
+	}
+
+	/**
+	* Show main gallery page
+	*/
+	function showMainGalleries() {
+		global $rsgConfig;
+		
+		$gallery =  rsgInstance::getGallery();
+		$this->gallery = $gallery;
+		
+		//Get values for page navigation from URL
+		$limit = JRequest::getInt( 'limitg', $rsgConfig->get('galcountNrs') );
+		$limitstart = JRequest::getInt( 'limitstartg', 0 );
+		//Get number of galleries including main gallery
+		$this->kids = $gallery->kids();
+		$kidCountTotal = count( $gallery->kids() );
+
+		$this->pageNav = false;
+		
+		//Show page navigation if selected in backend
+		if(( $rsgConfig->get('dispLimitbox') == 1 &&
+		    $kidCountTotal > $limit )  ||
+			$rsgConfig->get('dispLimitbox') == 2 )
+			{
+			require_once( JPATH_RSGALLERY2_ADMIN . DS . 'includes' . DS . 'gpagination.php' );
+			$this->kids = array_slice( $this->kids, $limitstart, $limit );
+			$this->pageNav = new JGPagination($kidCountTotal, $limitstart, $limit );
+
+		}
+		$this->display( 'gallery.php' );
+		
+	}
+
+
+    /***************************
+		non page public functions
+	***************************/
+	/**
+	 * Shows the gallery details block when set in the backend
+	 */
+	function _showGalleryDetails( $kid ) {
+		global $rsgConfig;
+		$slideshow = $rsgConfig->get('displaySlideshow') && $kid->itemCount() > 1;
+		$owner 		= $rsgConfig->get('showGalleryOwner');
+		$size 		= $rsgConfig->get('showGallerySize');
+		$date 		= $rsgConfig->get('showGalleryDate');
+		
+		//Check if items need to be shown
+		if ( ($slideshow + $owner + $size + $date) > 0 ) {
+			?>
+			<div class="rsg_gallery_details">
+			<div class="rsg2_details">
+			<?php
+			if ($slideshow) {
+				?>
+				<a href='<?php echo JRoute::_("index.php?option=com_rsgallery2&page=slideshow&gid=".$kid->get('id')); ?>'>
+				<?php echo JText::_('COM_RSGALLERY2_SLIDESHOW'); ?></a><br />
+				<?php
+			}
+			
+			if ($owner) {
+				echo JText::_('COM_RSGALLERY2_OWNER-'); echo $kid->owner;?><br />
+				<?php
+			} 
+			
+			if ($size) {
+				echo JText::_('COM_RSGALLERY2_SIZE-'); echo galleryUtils::getFileCount($kid->get('id')). ' ' . JText::_('COM_RSGALLERY2_IMAGES');?><br />
+			<?php
+			}
+			
+			if ($date) {
+				echo JText::_('COM_RSGALLERY2_CREATED-').""; echo JHTML::_("date", $kid->date,JText::_('COM_RSGALLERY2_DATE_FORMAT_LC3') );
+				?><br />
+				<?php
+			}
+			?>
+			</div>
+			</div>
+			<?php
+		}
+	}
+    
+    /***************************
+		private functions
+	***************************/
+
+    /**
+     * @todo this alternate gallery view needs to be moved to an html file and added as a template parameter
+     */
+    function _showDouble( $kids ) {
+		global $rsgConfig;
+        $i = 0;
+		echo"<div class='rsg_double_fix'>";
+        foreach ( $kids as $kid ) {
+			$i++;
+            ?>
+            <div class="rsg_galleryblock_double_<?php echo $i?>">
+				<div class="rsg2-galleryList-status"><?php echo $kid->status;?></div>
+				<div class="rsg2-galleryList-thumb_double">
+					<?php echo $kid->thumbHTML; ?>
+				</div>
+				<div class="rsg2-galleryList-text_double">
+					<?php echo $kid->galleryName;?>
+					<span class='rsg2-galleryList-newImages'>
+						<sup><?php echo galleryUtils::newImages($kid->get('id')); ?></sup>
+					</span>
+					<?php echo $this->_showGalleryDetails( $kid );?>
+					<div class="rsg2-galleryList-description"><?php echo $kid->description;?>
+					</div>
+				</div>
+				<div class="rsg_sub_url"><?php $this->_subGalleryList( $kid ); ?>
+				</div>
+			</div>
+            <?php
+			if($i>1){
+				$i = 0;
+			}
+        }
+		echo "</div>";
+    }
+    
+    /**
+     * @todo this alternate gallery view needs to be moved to an html file and added as a template parameter
+     */
+    function _showBox( $kids, $subgalleries ) {
+        ?>
+		<div class="rsg_box_block">
+            <?php
+            $i = 0;
+            foreach ( $kids as $kid ) {
+                $i++;
+				if($i>3){
+					$i = 1;
+					}
+			 ?>
+                <div class="rsg_box_box_<?php echo $i;?>">
+                    <div class="rsg_galleryblock">
+                    	<div>
+							<div class="rsg2-galleryList-status"><?php echo $kid->status; ?></div>
+                            <?php echo $kid->galleryName;?>
+                            <sup><span class='rsg2-galleryList-newImages'><?php echo galleryUtils::newImages($kid->get('id')); ?></span></sup>
+                            <div class='rsg2-galleryList-totalImages'>(<?php echo galleryUtils::getFileCount($kid->get('id')).' '. JText::_('COM_RSGALLERY2_IMAGES');?>)</div>
+                        </div>
+						<div>
+                        	<div class="rsg2-galleryList-thumb_box">
+								<?php echo $kid->thumbHTML; ?>
+                        	</div>
+                        	<div class="rsg2-galleryList-text_box">
+                          		<?php echo $this->_showGalleryDetails( $kid );?>
+                        	</div>
+                    	</div>
+                        <div class="rsg2-galleryList-description_box">
+                            	<?php echo $kid->description;?>
+						</div>
+                        <div class="rsg_sub_url">
+                        		<?php $this->_subGalleryList( $kid ); ?> 
+                        </div>
+                    </div>
+                </div>
+                <?php
+            }
+			?>
+            </div>
+        <?php
+    }
+
+	/**
+	 * Shows thumbnails for gallery
+	 */
+	function showThumbs() {
+		global $rsgConfig;
+		$my = JFactory::getUser();
+
+		$itemCount = $this->gallery->itemCount();
+
+		$limit = $rsgConfig->get("display_thumbs_maxPerPage") ;
+		$limitstart = JRequest::getInt( 'limitstart' );
+		
+		//instantiate page navigation
+		jimport('joomla.html.pagination');
+		$pagenav = new JPagination( $itemCount, $limitstart, $limit );//MK gaat goed: thumbs in gallery
+	
+		// increase the gallery hit counter
+		$this->gallery->hit();
+		
+		if( !$this->gallery->itemCount() ){
+			if( $this->gallery->id ){
+				// if gallery is not the root gallery display the message
+				echo JText::_('COM_RSGALLERY2_NO_IMAGES_IN_GALLERY');
+			}
+			// no items to display, so we can return;
+			return;
+		}
+		//Old rights management. If user is owner or user is Super Administrator, you can edit this gallery
+		if(( $my->id <> 0 ) and (( $this->gallery->uid == $my->id ) OR ( $my->usertype == 'Super Administrator' )))
+			$this->allowEdit = true;
+		else
+			$this->allowEdit = true;
+
+		switch( $rsgConfig->get( 'display_thumbs_style' )){
+			case 'float':
+				$this->display( 'thumbs_float.php' );
+			break;
+			case 'table':
+				$this->display( 'thumbs_table.php' );
+			break;
+		}
+		?>
+		<div class="pagination">
+				<?php
+				if( $itemCount > $limit ){
+					echo $pagenav->getPagesLinks();
+					echo "<br /><br />".$pagenav->getPagesCounter();
+				}
+				?>
+		</div>
+		<?php
+	}
+    
+    /**
+     * Shows main item
+     */
+	function showItem(){
+		global $rsgConfig;
+		
+		$item = rsgInstance::getItem();
+
+    	// increase hit counter
+		if (is_object($item)){	//Can this be achieved in a better way? When an item is unpublished (there is no $item object) a user gets a "Call to a member function hit() on a non-object" error without this check. With Joomla SEF we get a Notice "Could not find an image with image id ." (without the id number) and without Joomla SEF we get a blank page?!
+			$item->hit();
+		}
+		
+		?>
+		<table border="0" cellspacing="0" cellpadding="0" width="100%">
+			<tr>
+				<td><h2 class='rsg2_display_name' align="center"><?php echo htmlspecialchars(stripslashes($item->title), ENT_QUOTES); ?></h2></td>
+			</tr>
+			<tr>
+				<td>
+				<div align="center">
+					<?php
+					$this->currentItem = $item;
+					$this->display("item_" .  $item->type . ".php");
+    				?>
+				</div>
+				</td>
+			</tr>
+			<tr>
+				<td><?php $this->_writeDownloadLink( $item->id );?></td>
+			</tr>
+		</table>
+		<?php
+	}
+ 
+	/**
+	 * Show page navigation for Display image
+	 */
+	function showDisplayPageNav() {//MK this is where the images are shown with limit=1
+		$gallery = rsgGalleryManager::get();
+		$itemId = JRequest::getInt( 'id', 0 );
+		if( $itemId != 0 ){
+			// if the item id is set then we need to set the gid instead
+			// having the id variable set in the querystring breaks the page navigation
+
+			// i have not found any other way to remove a query variable from the router
+			// JPagination uses the router to build the current route, so removing it from the 
+			// request variables only does not work.
+			$app	= &JFactory::getApplication();
+			$router = &$app->getRouter();
+
+			$router->setVar('gid',$gallery->id);
+			$router->setVar('id',Null);				//unsets the var id from JRouter
+			
+			// set the limitstart so the pagination knows what page to start from
+			$itemIndex = $gallery->indexOfItem($itemId);
+			$router->setVar("limitstart", $itemIndex);
+			JRequest::setVar('limitstart', $itemIndex);
+		}
+
+		$pageNav = $gallery->getPagination();	
+		$pageLinks = $pageNav->getPagesLinks(); 
+
+		?>
+		<div align="center">
+		 <div class="pagination">
+			<?php echo $pageLinks; ?>
+		 </div>
+		</div>
+		<?php
+	}
+
+	/**
+	 * Shows details of image
+	 */
+	function showDisplayImageDetails() {
+		global $rsgConfig;
+		
+		$gallery = rsgGalleryManager::get();
+
+		// if no details need to be displayed then exit
+		
+		if (! ( $rsgConfig->get("displayDesc") || $rsgConfig->get("displayVoting") || $rsgConfig->get("displayComments") || $rsgConfig->get("displayEXIF") ))
+			return;
+
+		jimport("joomla.html.pane");
+		
+		$tabs =& JPane::getInstance("Tabs",array("useCookies" => true));
+		echo $tabs->startPane( 'tabs' );
+		
+		if ( $rsgConfig->get("displayDesc") ) {
+			echo $tabs->startPanel(JText::_('COM_RSGALLERY2_DESCRIPTION'), 'rs-description' );
+			$this->_showDescription(); 
+			echo $tabs->endPanel();
+		}
+		
+		if ( $rsgConfig->get("displayVoting") ) {
+			echo $tabs->startPanel(JText::_('COM_RSGALLERY2_VOTING'), 'Voting' );
+			$this->_showVotes();
+			echo $tabs->endPanel();
+		}
+		
+		if ( $rsgConfig->get("displayComments") ) {
+			echo $tabs->startPanel(JText::_('COM_RSGALLERY2_COMMENTS'), 'Comments' );
+			$this->_showComments();
+			echo $tabs->endPanel();
+		}
+	
+		if ($rsgConfig->get("displayEXIF") ) {
+			echo $tabs->startPanel(JText::_('COM_RSGALLERY2_EXIF'), 'EXIF' );
+			$this->_showEXIF();
+			echo $tabs->endPanel();
+		}
+		echo $tabs->endPanel();
+		
+	}
+
+    /**
+     * Show description
+     */
+	function _showDescription( ) {
+		global $rsgConfig;
+		$item = rsgInstance::getItem();
+		
+		if( $rsgConfig->get('displayHits')):
+		?>
+		<p class="rsg2_hits"><?php echo JText::_('COM_RSGALLERY2_HITS'); ?> <span><?php echo $item->hits; ?></span></p>
+		<?php
+		endif;
+		
+		if ( $item->descr ):
+		?>
+		<p class="rsg2_description"><?php  echo stripslashes($item->descr); ?></p>
+		<?php
+		endif;
+	}
+
+	
+	/**
+	* list sub galleries in a gallery
+	* @param rsgGallery parent gallery
+	*/
+	function _subGalleryList( $parent ){
+		$kids = $parent->kids();
+
+		if( count( $kids ) == 0 ) return;
+		
+		echo JText::_('COM_RSGALLERY2_SUBGALLERIES');
+		
+		$kid = array_shift( $kids );
+		
+		while( true ){
+			?>
+			<a href="<?php echo JRoute::_("index.php?option=com_rsgallery2&gid=".$kid->id); ?>">
+				<?php echo htmlspecialchars(stripslashes($kid->name), ENT_QUOTES); ?>
+				(<?php echo $kid->itemCount(); ?>)</a><?php
+
+			if( $kid = array_shift( $kids ))
+				echo ', ';
+			else
+				break;
+		}
+	}
+}
