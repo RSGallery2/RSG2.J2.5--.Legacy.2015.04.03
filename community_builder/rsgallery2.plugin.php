cefef1cc562d159e114efc7e7a672fe686049481
mirjam <mirjam@179cf1bb-832e-0410-8d33-ab44d92326cd>
2012-01-12 17:09:06 +0000

    Plugin for Community Builder to show tab of RSGallery2 galleries that are owned by current user. Works with J!1.7.

--- community_builder/rsgallery2.plugin.php
+++ community_builder/rsgallery2.plugin.php
@@ -1,7 +1,9 @@
 <?php
 /**
-* Tab to display the link to the RSGallery2 edit page in Community Builder
+* Tab to display the link to the RSGallery2 edit page in Community Builder (User profile, only published galleries owned by the user that is logged in)
 * Author: Dave Ludwigsen (palamangelus) dave@daveludwigsen.com
+* Changes for Joomla 1.5 Native by RSGallery2 Team (9 January 2012)
+* Changes for Joomla 1.7 by RSGallery2 Team (9 January 2012)
 */
 
 class getrsgallery2Tab extends cbTabHandler 
@@ -13,81 +15,85 @@ class getrsgallery2Tab extends cbTabHandler
 	
 	function getDisplayTab($tab,$user,$ui) 
 	{
-		global $database, $mosConfig_live_site, $mosConfig_absolute_path, $mosConfig_lang;
-	
-		//$params = $this->params;
-		//$eventLimit = $params->get('rsgallery2Link','');
-
-		//$query = "SELECT id FROM #__uhp WHERE user_id='".$user->id."'";
-		//$database->setQuery($query);
-		//$uhpid = $database->loadResult();
-	
 		$return=$this->ShowUserGalleries($user);
-
 		return $return;
 	}
 
 	function ShowUserGalleries($user)
 	{
-		global $my, $database, $mosConfig_live_site, $Itemid;
+		$database = JFactory::getDBO();
+		$my =& JFactory::getUser();
+		// Get the Itemid of the active menu item (currently the Community Builder item, not the RSGallery2 menu-item (since that can be multiple items and there is no way to tell which one the user wants)
+		$menu = &JSite::getMenu();
+		$active = $menu->getActive();
+		$Itemid = $active->id;
 
+		// Get the RSGallery2 configuration
+		require_once(JPATH_ROOT. DS .'administrator' . DS . 'components' . DS . 'com_rsgallery2' . DS . 'includes' . DS . 'config.class.php');
+		$rsgConfig = new rsgConfig();
+
+		// Check if My Galleries is enabled
+		$myGalleries = $rsgConfig->get('show_mygalleries');
+
+		//Build the tab for Community Builder
 		$htmlText = "";
-
 		$htmlText .= "<div class=\"sectiontableheader\" style=\"text-align:left;padding-left:0px;padding-right:0px;margin:0px 0px 10px 0px;height:auto;width:100%;\">";		
-		
-		//if this user, show edit galleries link
-		if ($my->id == $user->id)
-		{
-			$htmlText.="<div align=left><a href=\"".$mosConfig_live_site."/index.php?option=com_rsgallery2&Itemid=".$Itemid."&page=my_galleries\">Edit Gallery Items</a></div><br>";
-		}
-		else
-		{
+
+		//if this user, show edit galleries link AND if My Galleries is enabled
+		if (($my->id == $user->id) AND $myGalleries) {
+			$htmlText.="<div align=left><a href=\"".JUri::base()."index.php?option=com_rsgallery2&Itemid=".$Itemid."&rsgOption=myGalleries\">Edit Gallery Items</a></div>";
+		} else {
 			$htmlText.="<div align=left>Gallery Items</div><br>";
 		}
 
-		
 		//Prepare query for gallery list
-
-		//$htmlText .= "<div class=\"sectiontableheader\" style=\"text-align:left;padding-left:0px;padding-right:0px;margin:0px 0px 10px 0px;height:auto;width:100%;\">";
-		//$htmlText .= "Galleries<br>";
+		$query = $database->getQuery(true);
+		$query->select('*');
+		$query->from('#__rsgallery2_galleries');
+		$query->where('published = 1');
+		$query->where('uid = '. (int) $user->id);
+		$query->order('ordering ASC');
+		$database->setQuery((string)$query);
+		//Gallery list
+		$galleries = $database->loadObjectList();
 		
-		$database->setQuery("SELECT * FROM #__rsgallery2_galleries WHERE published=1 AND uid=".$user->id." ORDER BY ordering ASC");
-		//$htmlText.="SELECT * FROM #__rsgallery2_galleries WHERE published=1 AND parent=0 AND uid=".$my->id." ORDER BY ordering ASC";
 		//Loop through the galleries
-     		$galleries=$database->loadObjectList();
-
-			$i = 2;
-			
-	        foreach ($galleries as $gallery) 
-		{
+		$i = 2;	//Help for alternating styling: sectiontableentry1 and sectiontableentry2
+		foreach ($galleries as $gallery) {
 			$i = ($i==1) ? 2 : 1;
 			$htmlText.= "<div class=\"sectiontableentry$i\">";
-			$htmlText.="<a href=\"index.php?option=com_rsgallery2&Itemid=".$Itemid."&catid=".$gallery->id."\">";
+			$htmlText.="<a href=\"index.php?option=com_rsgallery2&Itemid=".$Itemid."&gid=".$gallery->id."\">";
 
-			//get the first image for the gallery
-			//images/rsgallery/thumb/
-			$database->setQuery("SELECT * FROM #__rsgallery2_files WHERE gallery_id=".$gallery->id." LIMIT 1");
-			//$htmlText.="SELECT * FROM #__rsgallery2_files WHERE gallery_id=".$gallery->id." LIMIT 1";
+			// Get either a random image or the assigned gallery thumbnail			
+			$query = $database->getQuery(true);
+			$query->select('*');	//
+			$query->from('#__rsgallery2_files');
+			$query->where('gallery_id = '. (int) $gallery->id);
+			if ($gallery->thumb_id) {
+				$query->where('id = '. (int) $gallery->thumb_id);	//assigned thumbnail
+			} else {
+				$query->order('RAND()');	//random image
+			}
+			$database->setQuery((string)$query, 0, 1);	//Limit the list to one item
+			// Get the thumbnail
 			$thumbnails=$database->loadObjectList();
 
 			//leave room in case we later want to expand for more thumbnails.
-			foreach ($thumbnails as $thumbnail)
-			{
-				if ($thumbnail->name == "")
+			foreach ($thumbnails as $thumbnail) {
+				if ($thumbnail->name == "") {
 					break;
-				else
-				{
+				} else {
 					$htmlText.="<img src=\"images/rsgallery/thumb/".$thumbnail->name.".jpg\" border=0 alt=\"".$thumbnail->descr."\" valign=absmiddle> ";
 					break;
 				}
 			}
 
 			$htmlText.=$gallery->name;
-			$htmlText.="</a><br />";
+			$htmlText.="</a></div>";
 		}
-		$htmlText.="</div></div></div>";	
+		$htmlText.="</div>";	
+		
 		return $htmlText;
 	}
-
 }	
 ?>
