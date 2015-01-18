21c0a50a3ec7849edf8eb2150f97f7ea547ca70b
thomasfinnern <thomasfinnern@179cf1bb-832e-0410-8d33-ab44d92326cd>
2015-01-18 09:57:27 +0000

    11.01.2015 finnern Version 04.2.3 + Slideshow parth is working + Begun with adding PHP doc but may be wrong in some descriptions + Changed ... die('Restricted access'); to ... or die(); 	Joomla wants not to give an intruder any information + Adjusted slidshow parth to mootools 1.4.5 (Removed 1.11 files in folder)

--- com_rsgallery2/branches/towardsJ3_PlanB/admin/includes/config.class.php
+++ com_rsgallery2/branches/towardsJ3_PlanB/admin/includes/config.class.php
@@ -147,7 +147,7 @@ class rsgConfig {
         // global $rsgVersion;
         // $this->version = $rsgVersion->getVersionOnly();
         //$this->version = '3.2.0';
-        $this->version = '4.0.2';
+        $this->version = '4.0.3';
 
         if( $loadFromDB )
             $this->_loadConfig();
